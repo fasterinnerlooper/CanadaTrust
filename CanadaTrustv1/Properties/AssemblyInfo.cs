5fd1bd071d54e439b39ec8040a4a40fb95a2b641
sjetha <shafiq.jetha@gmail.com>
2013-10-28 08:22:27 -0600

    Updated name and version information

--- CanadaTrustv1/Properties/AssemblyInfo.cs
+++ CanadaTrustv1/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CanadaTrustv1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TD Branch Locator")]
+[assembly: AssemblyDescription("Locator for TD Branches")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CanadaTrustv1")]
+[assembly: AssemblyCompany("Shafiq Jetha")]
+[assembly: AssemblyProduct("TD Branch Locator")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,4 +34,4 @@ using System.Resources;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: NeutralResourcesLanguageAttribute("en-CA")]
