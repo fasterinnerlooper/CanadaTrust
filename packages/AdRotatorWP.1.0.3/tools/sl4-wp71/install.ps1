259955e3ef0ca9b9ec07152c782eb6a26389c445
sjetha <shafiq.jetha@gmail.com>
2013-11-08 21:55:32 -0700

    Removed AdRotator Package

--- packages/AdRotatorWP.1.0.3/tools/sl4-wp71/install.ps1
+++
@@ -1,9 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-$item = $project.ProjectItems | where-object {$_.Name -eq "defaultAdSettings.xml"}
-
-$item.Properties.Item("BuildAction").Value = [int]8
-
-$item2 = $project.ProjectItems | where-object {$_.Name -eq "ReadMe-AdRotator-XAML.txt"}
-
-$item2.Properties.Item("BuildAction").Value = [int]0
