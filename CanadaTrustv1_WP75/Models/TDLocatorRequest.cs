c377fa5964a3654952d230a48b9203b342ae8975
sjetha <shafiq.jetha@gmail.com>
2014-04-27 10:01:55 -0600

    Updated variables used in TDLocatorRequest

--- CanadaTrustv1_WP75/Models/TDLocatorRequest.cs
+++ CanadaTrustv1_WP75/Models/TDLocatorRequest.cs
@@ -14,14 +14,16 @@ namespace CanadaTrustv1.Models
     public class TDLocatorRequest
     {
         private string requestURI = "http://td.via.infonow.net/locator/AdvancedSearchAction.do";
-        private string fullAddress = "16+Ave+NE%2C+Calgary%2C+AB+T2E%2C+Canada";
+        private string fullAddress = null;
         public string FullAddress
         {
-            get { return fullAddress; }
+            get
+            {
+                return fullAddress;
+            }
             set
             {
                 fullAddress = HttpUtility.UrlEncode(value);
-                fullAddress = fullAddress;
             }
         }
 
@@ -29,7 +31,8 @@ namespace CanadaTrustv1.Models
         {
         }
 
-        public Uri compileUri() {
+        public Uri compileUri()
+        {
             string querystring = "dummyFullAddress=" + fullAddress + "&branchSearch=Branch&branchSearch=&abmSearch=Abm&abmSearch=&searchCustom__openSat=&searchCustom__openSun=&searchCustom__coinCounter=&searchCustom__openAfterSix=&searchCustom__openSunPresent=FALSE&searchCustom__openSatPresent=TRUE&searchCustom__openSunPresent=FALSE&searchCustom__wheelType=&fullAddress=" + fullAddress + "&originalSearchValue=&country=Canada&mapAndList=mapAndList&searchCustom__transitNumber=&searchCustom__searchBy=&.x=29&.y=6";
             return new Uri(requestURI + "?" + querystring);
         }
