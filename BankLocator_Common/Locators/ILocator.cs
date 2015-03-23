25d73bce1573a1de6288789a34a6a8735ace2e5c
sjetha <shafiq.jetha@gmail.com>
2015-03-22 21:53:32 -0600

    Created ILocator interface

---
+++ BankLocator_Common/Locators/ILocator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace BankLocator_Common.Locators
+{
+    interface ILocator
+    {
+        System.Threading.Tasks.Task BeginHttpClientRequest();
+        System.Collections.Generic.List<BankLocator_Common.Models.BMOBranch> Branches { get; set; }
+        System.Threading.Tasks.Task CreateBranches();
+        System.Net.Http.HttpClient HttpClient { get; }
+        System.Net.Http.StringContent HttpContent { get; }
+        System.Net.Http.HttpResponseMessage HttpResponseMessage { get; }
+        void InitializeHttpContent();
+        double Latitude { get; set; }
+        double Longitude { get; set; }
+        void SetLocation(double latitude, double longitude);
+        BankLocator_Common.Models.RootObject translateJson(string json);
+    }
+}
